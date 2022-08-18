86cb695b0f74ae80aa14ac49c4c2e84b06797b41
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2022-08-17 21:39:46 -0400

    choices update

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 let posts;
-fetch("https://www.reddit.com/r/aww/.json").then(response => response.json()).then(data => {
-    posts = data.data.children;
-    for(i = 0; i < 10; i++){
-        let title = document.createElement("h1");
-        title.innerText = posts[i].data.title;
-        let url = document.createElement("a");
-        url.innerText = "Click here to view OP's post!"
-        url.href = posts[i].data.url;
-        let thumbnail = document.createElement("img");
-        thumbnail.src = posts[i].data.thumbnail;
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("post");
-        newDiv.appendChild(title);
-        newDiv.appendChild(url);
-        newDiv.appendChild(thumbnail);
-        document.getElementById("postDisplay").appendChild(newDiv);
+let subreddit;
+document.getElementById("submit").addEventListener("click", (event) =>{
+    subreddit = document.getElementById("subreddit").value;
+    document.getElementById("title").innerText = `Welcome to r/${subreddit}`;
+    while(document.getElementById("postDisplay").hasChildNodes()){
+        document.getElementById("postDisplay").firstChild.remove();
     }
+    fetch(`https://www.reddit.com/r/${subreddit}/.json`).then(response => response.json()).then(data => {   
+        posts = data.data.children;
+        for(i = 0; i < 10; i++){
+            let title = document.createElement("h1");
+            title.innerText = posts[i].data.title;
+            let url = document.createElement("a");
+            url.innerText = "Click here to view OP's post!"
+            url.href = posts[i].data.url;
+            let thumbnail = document.createElement("img");
+            thumbnail.src = posts[i].data.thumbnail;
+            let newDiv = document.createElement("div");
+            newDiv.classList.add("post");
+            newDiv.appendChild(title);
+            newDiv.appendChild(url);
+            newDiv.appendChild(thumbnail);
+            document.getElementById("postDisplay").appendChild(newDiv);
+        }
+    });
 });
-
